25eb207d7859c132023de3bd8ad1e9090d040148
DictumMortuum <theio.vrefos@gmail.com>
2017-03-25 02:25:59 +0200

    renamed let to var

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 
 module.exports = {
   startOfDay: function(d) {
-    let t = new Date(d);
+    var t = new Date(d);
     t.setHours(0, 0, 0, 0);
     return t;
   },
   endOfDay: function(d) {
-    let t = new Date(d);
+    var t = new Date(d);
     t.setHours(23, 59, 59, 999);
     return t;
   },
   daysAgo: function(d, n) {
-    let t = new Date(d);
+    var t = new Date(d);
     t.setDate(d.getDate() - (n || 30));
     return t;
   }
